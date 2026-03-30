952edf11cfb7ae6325fcfabefbda699b02b72632
Stereopapa <72004744+Stereopapa@users.noreply.github.com>
2026-03-30 21:26:44 +0200

    add: FIR filter implementation to x-64 assembly

--- asm_x-64/main.c
+++ asm_x-64/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void print_array(int* tab, int n) {
+	printf("[ ");
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", tab[i]);
+	}
+	printf("]\n");
+}
+
 extern __int64 find_max(__int64* tab, __int64 n);
 void find_max_task() {
 	__int64 results[12] = { -15, 4000000, -345679, 88046592,
@@ -12,8 +21,39 @@ void find_max_task() {
 	printf("\nMax: %I64d\n", max_val);
 }
 
+extern void fir_filter(const int N, const int L, int* input, int* output, int* coefs, int* delay);
+void fir_filter_task() {
+#define N 32
+#define L 16
+	int input[N] = {
+	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+	0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14,
+	0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E,
+	0x1F, 0x20
+	};
+	int output[N] = { 0 };
+	int delay[L] = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	};
+
+	int coefs[L] = {
+		1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1
+	};
+
+	fir_filter(N, L, input, output, coefs, delay);
+	printf("Coefs\n");
+	print_array(coefs, L);
+	printf("Input\n");
+	print_array(input, N);
+	printf("Output\n");
+	print_array(output, N);
+
+}
+
 
 int main() {
-	find_max_task();
+	//find_max_task();
+	fir_filter_task();
 	return 0;
 }
--- asm_x-64/task.asm
+++ asm_x-64/task.asm
@@ -1,4 +1,5 @@
 public find_max
+public fir_filter
 
 .code
 find_max PROC
@@ -8,24 +9,106 @@ find_max PROC
 	mov rbx, rcx
 	mov rcx, rdx
 	test rcx, rcx
-	jz end
+	jz fm_end
 
 	mov rsi, 0 ;index in array
 	mov rax, [rbx + 8*rsi] ;first element
 
 @@:
 	dec rcx
-	jz end
+	jz fm_end
 	inc rsi
 	cmp rax, [rbx + 8*rsi]
 	jge next
 	mov rax, [rbx + 8*rsi]
 next: jmp @b
 
-end:
+fm_end:
 
 	pop rsi
 	pop rbx
 	ret
 find_max ENDP
+
+fir_filter PROC
+	; RCX = N
+	; RDX = L
+	; R8 = input
+	; R9 = output
+	; [RBP+48] = coefs
+	; [RBP+56] = delay
+
+	push rbp
+	mov rbp,rsp
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	
+	
+	;   r8 =	 ;input
+	;   r9 =	 ;output
+	mov r10, rcx ;N
+	mov r11, rdx ;L
+	mov r12, [RBP+56] ;delay pos pointer
+	mov r13, r12 ;delay base adress
+	mov r14, [RBP+48] ;coefs
+	;   r15 =    ;accumulator
+	;	rcx=	 ;Convolution loop counter
+	;	rax=	 ;temp1
+
+	y_loop:
+		;Delay pointer adjustment
+		
+		cmp r12, r13
+		ja outer_delay_in_memory_range
+		lea r12, [r13+4*r11]
+		outer_delay_in_memory_range:
+		sub r12, 4
+		;Saving another sample into delay
+		mov eax, [r8]
+		add r8, 4
+		mov dword ptr [r12], eax
+
+		xor r15, r15
+		xor rcx, rcx
+		sum:
+			;calculating convolution
+			;there is adventage to not use rcx for addr of delay pointer calc 
+			;because if we adjust it by the end of convolution loop 
+			;we end up on correct memory addr to add another sample to delay
+			mov eax, [r12] 
+			imul eax, [r14+4*rcx]
+			add r15d, eax
+			;Delay pointer adjustment
+			add r12, 4
+			lea rax, [r13+r11*4]
+			cmp r12, rax
+			jb inner_delay_in_memory_range
+			mov r12, r13 ;if on the end go back to start
+			inner_delay_in_memory_range:
+
+			inc rcx
+			cmp rcx, r11
+			jb sum ;i = 0, rcx = 1, i=15, rcx = L, rcx !< L 
+		;Saving output to memory
+		mov [r9], r15d
+		add r9, 4
+		
+		;outer loop iteration
+		dec r10
+		jnz y_loop
+
+	
+
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
+	ret
+fir_filter ENDP
+
 END
